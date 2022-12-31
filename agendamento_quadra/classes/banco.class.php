e218039109bc5bd9b8136bc29c028a260540f52b
AndroidHackathon <lucassanches12398@hotmail.com>
2022-12-31 12:32:14 -0300

    tentando adicionar um filtro nas pesquisas, ainda não esta funcionando 100%, esta dando erro ainda caso depois da consulta o usuário selecionar nada.

--- agendamento_quadra/classes/banco.class.php
+++ agendamento_quadra/classes/banco.class.php
@@ -67,19 +67,17 @@ class quadra
 
     public function validaAgendamento($id,$data,$hora){
 
-        $sql = "SELECT * FROM agendamento where id_local = '$id' and data_agendamento = '$data' and hora = '$hora'";
+        $sql = "SELECT * FROM agendamento where id_local = '$id' and data_agendamento = '$data' and hora = $hora";
         $statement = $this->conexao->prepare($sql);
         $statement->execute();
-        $array = $statement->fetchAll(PDO::FETCH_ASSOC);
         $rows = $statement->rowCount();
 
         if($rows){
             echo '<script>alert("Esse campo/quadra já esta registrado nesse horário");</script>';
-            
-        }   else{
+        }else{
             $cpf = $_SESSION['cnpj_cpf']; 
 
-            $sql = "INSERT INTO agendamento VALUES (0,'$cpf','$data',$id,1,'$hora')";
+            $sql = "INSERT INTO agendamento VALUES (0,'$cpf','$data',$id,1,$hora)";
             $statement = $this->conexao->prepare($sql);
             $resultado = $statement->execute();
             if ($resultado) {
@@ -88,8 +86,32 @@ class quadra
                 echo '<script>alert("Erro no registro!");</script>';
                 }
         }
-
     }
 
+    public function readQuioesqueFilter($local_origem_consulta,$data_agendamento_consulta)
+    {
+        $sql = 
+        "SELECT a.* 
+            ,l.local_origem
+        from agendamento a
+        left join local l on l.id = a.id_local  
+        left join tipo_local t on t.id = l.id_tipo_local
+
+        where t.descricao = 'Campos'
+        and a.id_situacao = 1
+        and a.data_agendamento >= CURDATE()
+        and a.id_local = $local_origem_consulta
+        and a.data_agendamento = $data_agendamento_consulta
+        order by a.data_agendamento, l.local_origem";
+
+        $statement = $this->conexao->prepare($sql);
+        $statement->execute();
+        $array = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $array;
+    }
+
+
+
+    
 }
 
