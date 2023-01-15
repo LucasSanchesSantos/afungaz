4d0f11401cab0d79d130f054cc165345221ced68
Lucas Sanches dos Santos <lucas.santos@gazin.com.br>
2023-01-14 21:36:00 -0300

    Adicionando o padrão utf8 em todas as classes

--- class_start/banco.class.create.php
+++ class_start/banco.class.create.php
@@ -16,6 +16,8 @@ class create
         try {
             $this->conexao = new PDO("mysql:host=$this->host;
             dbname=$this->database", "$this->user", "$this->password");
+            $this->conexao->exec("set names utf8mb4");
+
         } catch (\PDOException $e) {
             echo "Não foi possível estabelecer a conexão 
             com o banco de dados: Erro" . $e->getCode();
